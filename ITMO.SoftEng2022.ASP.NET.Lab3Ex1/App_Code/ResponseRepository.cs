ce2329596ed400a4b543f31fceef7dbda9895e34
Egorov Dmitry <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-10-02 19:23:09 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.ASP.NET.Lab3Ex1/App_Code/ResponseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// Создание хранилища данных
+
+namespace AspNet
+{
+    public class ResponseRepository
+    {
+        private static ResponseRepository repository = new ResponseRepository();
+        private List<GuestResponse> responses = new List<GuestResponse>();
+        public static ResponseRepository GetRepository()
+        {
+            return repository;
+        }
+        public IEnumerable<GuestResponse> GetAllResponses()
+        {
+            return responses;
+        }
+        public void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
---
+++ ITMO.SoftEng2022.ASP.NET.Lab3Ex1/Reg.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// Упражнение 2.1.Обработка события загрузки формы 
+namespace AspNet
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // п.2
+            if (IsPostBack)
+            {
+                GuestResponse rsvp = new GuestResponse(name.Text, email.Text, phone.Text, CheckBoxYN.Checked);
+                // п.3
+                ResponseRepository.GetRepository().AddResponse(rsvp);
+                // п.4
+                if (rsvp.WillAttend.HasValue && rsvp.WillAttend.Value)
+                {
+                    Response.Redirect("seeyouthere.html");
+                }
+                else
+                {
+                    Response.Redirect("sorryyoucantcome.html");
+                }
+
+
+            }
+
+        }
+    }
+}
